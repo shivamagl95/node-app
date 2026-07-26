75eda68e7c764a7ad81c59b9784436fd200940cc
Shivam Agrawal <shivam.agrawal@sleepcountry.ca>
2026-07-26 22:48:26 +0530

    add README.md in the app

--- terraform/infrastructure/aws/observability/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-bucket-shivam"
-    key            = "observability/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
--- terraform/infrastructure/aws/observability/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  env    = terraform.workspace
-  region = var.aws_region
-  vpc_name = "${local.env}-vpc"
-}
